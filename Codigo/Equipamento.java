2291e97f8ecc401ef653871cd2f8df518443683c
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-09-12 17:04:29 -0300

    Update Equipamento.java

--- Codigo/Equipamento.java
+++ Codigo/Equipamento.java
@@ -1,67 +1,49 @@
-public class Equipamento{
-    private int id;
-    private String descricao;
-    private String tipo;
-    private Double valor;
-    private int quantidadeDisponivel;
+public class Equipamento {
+	private int id;
+	private String descricao;
+	private String tipo;
+	private Double valor;
+	private int quantidadeDisponivel;
 
-  private Equipamento(){}
+	public Equipamento(int id, String descricao, String tipo, Double valor, int quantidadeDisponivel) {
+		this.id = id;
+		this.descricao = descricao;
+		this.tipo = tipo;
+		this.valor = valor;
+		this.quantidadeDisponivel = quantidadeDisponivel;
+	}
 
-  public Equipamento(int id, String descricao, String tipo, Double valor, int quantidadeDisponivel) {
-    this.id = id;
-    this.descricao = descricao;
-    this.tipo = tipo;
-    this.valor = valor;
-    this.quantidadeDisponivel = quantidadeDisponivel;
-  }
+	public int getId() {
+		return id;
+	}
 
-  public int getId() {
-    return id;
-  }
+	public String getDescricao() {
+		return descricao;
+	}
 
-  public void setId(int id) {
-    this.id = id;
-  }
+	public String getTipo() {
+		return tipo;
+	}
 
-  public String getDescricao() {
-    return descricao;
-  }
+	public Double getValor() {
+		return valor;
+	}
 
-  public void setDescricao(String descricao) {
-    this.descricao = descricao;
-  }
+	public int getQuantidadeDisponivel() {
+		return quantidadeDisponivel;
+	}
 
-  public String getTipo() {
-    return tipo;
-  }
 
-  public void setTipo(String tipo) {
-    this.tipo = tipo;
-  }
-
-  public Double getValor() {
-    return valor;
-  }
-
-  public void setValor(Double valor) {
-    this.valor = valor;
-  }
-
-  public int getQuantidadeDisponivel() {
-    return quantidadeDisponivel;
-  }
-
-  public void setQuantidadeDisponivel(int quantidadeDisponivel) {
-    this.quantidadeDisponivel = quantidadeDisponivel;
-  }
-
-  @Override
-  public String toString() {
-    return "Equipamento{" +
-           "id=" + id +
-           ", descricao='" + descricao + '\'' +
-           ", tipo='" + tipo + '\'' +
-           ", valor=" + valor +
-           '}';
-  }
+	@Override
+	public String toString() {
+		return "\nEquipamento:" 
+				+ "\nid=" 
+				+ id 
+				+ "\ndescricao: " 
+				+ descricao
+				+ "\ntipo: " 
+				+ tipo 
+				+ "\nvalor: R$"
+				+ valor;
+	}
 }
