658be26d32137ccdac151873629942bf444dded3
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-09-12 17:04:02 -0300

    Update Cliente.java

--- Codigo/Cliente.java
+++ Codigo/Cliente.java
@@ -1,42 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente{
-    private int id;
-    private String nome;
-    private List<Contrato> contratos = new ArrayList<>();
-  public Cliente(int id, String nome){
-    this.id = id;
-    this.nome = nome;
-  }
-  public int getId() {
-    return id;
-  }
+public class Cliente {
+	private int id;
+	private String nome;
+	private List<Contrato> contratos = new ArrayList<>();
 
-  public void setId(int id) {
-    this.id = id;
-  }
+	public Cliente(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
 
-  public String getNome() {
-    return nome;
-  }
+	public int getId() {
+		return id;
+	}
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
+	public List<Contrato> getContratos() {
+		return contratos;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
 
-  public List<Contrato> getContratos() {
-    return contratos;
-  }
 
-  public void setContratos(List<Contrato> contratos) {
-    this.contratos = contratos;
-  }
+	@Override
+	public String toString() {
+		return "Cliente{" + "id=" + id + ", nome='" + nome + '\'';
+	}
 
-  @Override
-  public String toString() {
-    return "Cliente{" +
-           "id=" + id +
-           ", nome='" + nome + '\'';
-  }
+
 }
