03368c5008be4af5c571acde704edc2d1ad22f29
Miguel Magalhaes Freire De Castro <104601913+MiguelMagCastro@users.noreply.github.com>
2023-09-19 19:23:48 -0300

    Update TestesUnitarios.java

--- Codigo/TestesUnitarios.java
+++ Codigo/TestesUnitarios.java
@@ -1,64 +1,92 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.time.LocalDate;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-class TestesUnitarios {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-	private Cliente cliente;
-	private Equipamento equipamento;
-	private Contrato contrato;
+public class TestesUnitarios {
 
-	@BeforeEach
-	public void defineValoresIniciais() {
-		cliente = new Cliente(1, "Miguel");
-		equipamento = new Equipamento(1, "Martelo", "Ferramentas", 10.0, 10);
-		contrato = new Contrato(1,cliente, equipamento, 2, LocalDate.now().plusDays(7));
-	}
+    private List<Cliente> clientes;
+    private List<Equipamento> equipamentos;
+    private List<Contrato> contratos;
+    
+    @Before
+    public void setUp() {
+    	clientes = new ArrayList<>();
+        equipamentos = new ArrayList<>();
+        contratos = new ArrayList<>();
+    }
 
-	@Test
-	public void verificaRegistroDeCliente() {
-		assertEquals(1, cliente.getId());
-		assertEquals("Miguel", cliente.getNome());
-	}
+    @After
+    public void tearDown() {
+        clientes = null;
+        equipamentos = null;
+        contratos = null;
+    }
+    
+    
+    @Test
+    public void testCadastraCliente() {
+        Controlador.sc = new java.util.Scanner("João");
+        Cliente cliente = Controlador.cadastraCliente();
+        assertEquals(1, cliente.getId());
+        assertEquals("João", cliente.getNome());
+        assertEquals(0, clientes.size());
+    }
 
-	@Test
-	public void verificaRegistroDeEquipamento() {
-		assertEquals(1, equipamento.getId());
-		assertEquals("Martelo", equipamento.getDescricao());
-		assertEquals("Ferramentas", equipamento.getTipo());
-		assertEquals(10.0, equipamento.getValor(), 0.001);
-		assertEquals(10, equipamento.getQuantidadeDisponivel());
-	}
+    @Test
+    public void testCadastraEquipamento() {
+    	Equipamento equipamento = new Equipamento(1, "Equipamento Teste", "Tipo Teste", 100.0, 10);
+        equipamentos.add(equipamento);
+        assertEquals(1, equipamentos.size());
+        Equipamento equipamentoEsperado = equipamentos.get(0);
+        assertEquals(1, equipamentoEsperado.getId());
+        assertEquals("Equipamento Teste", equipamentoEsperado.getDescricao());
+        assertEquals("Tipo Teste", equipamentoEsperado.getTipo());
+        assertEquals(100.0, equipamentoEsperado.getValor(), 0.01); 
+        assertEquals(10, equipamentoEsperado.getQuantidadeDisponivel());
+    }
+ 
+    
 
-	@Test
-	public void verificaCriacaoDoContrato() {
-		assertEquals(1,contrato.getId());
-		assertEquals(cliente, contrato.getCliente());
-		assertEquals(equipamento, contrato.getEquipamento());
-		assertEquals(2, contrato.getQuantidade());
-		assertEquals(LocalDate.now().plusDays(7), contrato.getDataFim());
-		assertEquals(140.0, contrato.getValorTotal(), 0.001);
-	}
+    @Test
+    public void testValidaDataInicio() {
+        Controlador.sc = new java.util.Scanner("31/12/2023");
+        LocalDate dataInicio = Controlador.validaDataInicio();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        assertEquals(LocalDate.parse("31/12/2023", formatter), dataInicio);
+    }
 
-	@Test
-	public void verificaValorTotalDoContrato() {
-		double valorEsperado = equipamento.getValor() * contrato.getQuantidade() * 7; // 1000 * 2 * 7 = 14000
-		assertEquals(valorEsperado, contrato.getValorTotal(), 0.001);
-	}
+    @Test
+    public void testValidaDataFim() {
+        Controlador.sc = new java.util.Scanner("01/01/2024");
+        LocalDate dataInicio = LocalDate.parse("01/01/2023", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalDate dataFim = Controlador.validaDataFim(dataInicio);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        assertEquals(LocalDate.parse("01/01/2024", formatter), dataFim);
+    }
 
-	@Test
-	public void verificaAtualizacaoDaQuantidadeDisponivel() {
-		int quantidadeContratada = contrato.getQuantidade();
-		equipamento.setQuantidadeDisponivel(quantidadeContratada);
-		int quantidadeEsperada = 10 - quantidadeContratada;
-		assertEquals(quantidadeEsperada, equipamento.getQuantidadeDisponivel());
-	}
+    @Test
+    public void testVerificaStatusDoContrato() {
+        LocalDate dataFimPassada = LocalDate.now().minusDays(1);
+        LocalDate dataFimFutura = LocalDate.now().plusDays(1);
 
-	@Test
-	public void verificaDataDeTerminoDoContrato() {
-		LocalDate dataFimEsperada = LocalDate.now().plusDays(7);
-		assertEquals(dataFimEsperada, contrato.getDataFim());
-	}
+        boolean statusPassado = Controlador.verificaStatusDoContrato(dataFimPassada.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        assertFalse(statusPassado);
+
+        boolean statusFuturo = Controlador.verificaStatusDoContrato(dataFimFutura.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        assertTrue(statusFuturo);
+    }
+    
+    
+    
+
 
 }
